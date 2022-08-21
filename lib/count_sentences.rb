4764f9dbbc10b7feb001cec88dc1389bb8dc4783
kennbeckloff <kennbeckloff688@gmail.com>
2022-08-21 10:53:17 +0300

    Done

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -2,19 +2,26 @@ require 'pry'
 
 class String
 
+  attr_accessor :string
+
   def sentence?
+  end_with? (".")
     
   end
 
   def question?
+  end_with?("?")
 
   end
 
   def exclamation?
+  end_with?("!")
 
   end
 
   def count_sentences
+    delimiters = ['?', '.','!']
+    split(Regexp.union(delimiters)).delete_if{|entered_word| entered_word== ""}.length
 
   end
 end
